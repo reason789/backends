d9c291046873258a8331b1ea169570c971d7de95
reason789 <rahadul134@gmail.com>
2023-06-08 23:17:41 +0600

    first commit done 2

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,38 +1,19 @@
-// const mongoose = require("mongoose");
-
-// const connectDatabase = () => {
-//   mongoose.set("strictQuery", false);
-//   mongoose
-//     .connect(process.env.DB_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     })
-//     .then((data) => {
-//       console.log(`Mongodb connected with server: `);
-//     });
-//   // .catch((err)=>{
-//   //     console.log(err)
-//   // })
-//   // because we handle the error in server.js
-// };
-
-// module.exports = connectDatabase;
-
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
   mongoose.set("strictQuery", false);
   mongoose
-    .connect(process.env.DB_URI, {
+    .connect(process.env.DB_URI.toString(), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
-    .then(() => {
-      console.log("MongoDB connected successfully.");
-    })
-    .catch((error) => {
-      console.error("MongoDB connection error:", error);
+    .then((data) => {
+      console.log(`Mongodb connected with server: `);
     });
+  // .catch((err)=>{
+  //     console.log(err)
+  // })
+  // because we handle the error in server.js
 };
 
 module.exports = connectDatabase;
