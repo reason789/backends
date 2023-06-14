fbc12c6f04a0f1c38a95e4549ac12145b53a9735
reason789 <rahadul134@gmail.com>
2023-06-14 18:24:38 +0600

    done

--- backend/app.js
+++ backend/app.js
@@ -24,16 +24,13 @@ app.use(
   })
 );
 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:3000", "https://seasidee.netlify.app"],
-//     methods: "GET,POST,PUT,DELETE",
-//     credentials: true,
-//     origin: true
-//   })
-// );
-
-app.use(cors({ credentials: true, origin: true }));
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "https://seasidee.netlify.app"],
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 
 const user = require("./routes/userRoute");
 const hotel = require("./routes/hotelRoute");
